5cc6ae4357660a05a2bdb0d45ec5c14bd83ffef8
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-04-14 12:00:21 -0300

    adicionando comentarios e renomeando a função printFamily para printChildren

--- arvores.go
+++ arvores.go
@@ -3,37 +3,37 @@ package datastructuresgo
 import "fmt"
 
 type Person struct {
-    Name     string
-    Age      int
-    Children []*Person
+	Name     string
+	Age      int
+	Children []*Person
 }
 
 func (p *Person) AddChild(child *Person) {
-    p.Children = append(p.Children, child)
+	p.Children = append(p.Children, child)
 }
 
 func (p *Person) PrintTree() {
-    fmt.Println(p.Name)
-    for _, child := range p.Children {
-        child.PrintTree()
-    }
-}
-
-func (p *Person) NumberOfChildren() int{
-	number := 0;
-	for _,  child := range p.Children {
-	 child.NumberOfChildren()
-	 return	number + 1;
+	fmt.Println(p.Name)
+	for _, child := range p.Children {
+		child.PrintTree()
 	}
-	return number;
 }
 
-func(p *Person) PrintFamily(){
-	fmt.Printf("\nNome:");
-	fmt.Println(p.Name,"\nIdade:",p.Age)
+func (p *Person) NumberOfChildren() int {
+	number := 0
+	for _, child := range p.Children {
+		child.NumberOfChildren()
+		return number + 1
+	}
+	return number
+}
+
+func (p *Person) PrintChildren() {
+	fmt.Printf("\nNome:")
+	fmt.Println(p.Name, "\nIdade:", p.Age)
 	PrintMessage("\nFilhos:")
-	for i := 0; i <=  p.NumberOfChildren(); i++{
-		fmt.Println(i + 1,"º Filho:",p.Children[i].Name,"\tIdade:",p.Children[i].Age)
+	for i := 0; i <= p.NumberOfChildren(); i++ {
+		fmt.Println(i+1, "º Filho:", p.Children[i].Name, "\tIdade:", p.Children[i].Age)
 	}
 }
 
@@ -41,28 +41,31 @@ func Trees() {
 
 	PrintMessage("\nÁRVORES:")
 
-    // Cria algumas pessoas
-    alice := &Person{Name: "Alice", Age: 70}
-    bob := &Person{Name: "Bob", Age: 55}
-    charlie := &Person{Name: "Charlie", Age: 25}
-    dave := &Person{Name: "Dave", Age: 10}
-    eve := &Person{Name: "Eve", Age: 10}
-    frank := &Person{Name: "Frank", Age: 5}
+	// Cria algumas pessoas
+	alice := &Person{Name: "Alice", Age: 70}
+	bob := &Person{Name: "Bob", Age: 55}
+	charlie := &Person{Name: "Charlie", Age: 25}
+	dave := &Person{Name: "Dave", Age: 10}
+	eve := &Person{Name: "Eve", Age: 10}
+	frank := &Person{Name: "Frank", Age: 5}
 	angelica := &Person{Name: "Ângelica", Age: 7}
 
-    // Adiciona filhos à árvore genealógica
-    alice.AddChild(bob)
-    alice.AddChild(charlie)
-    bob.AddChild(dave)
-    bob.AddChild(eve)
-    charlie.AddChild(frank)
+	// Adiciona filhos à árvore genealógica
+	alice.AddChild(bob)
+	alice.AddChild(charlie)
+	bob.AddChild(dave)
+	bob.AddChild(eve)
+	charlie.AddChild(frank)
 	charlie.AddChild(angelica)
 
-    // Imprime a árvore genealógica
-    alice.PrintTree()
+	// Imprime a árvore genealógica
+	alice.PrintTree()
 
-	alice.PrintFamily()
-	bob.PrintFamily()
-	charlie.PrintFamily()
-	
+	//Imprime os filhos de Alice
+	alice.PrintChildren()
+	//Imprime os filhos de Bob
+	bob.PrintChildren()
+	//Imprime os filhos de Charlie
+	charlie.PrintChildren()
+
 }
