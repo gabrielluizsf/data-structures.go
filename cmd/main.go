52ac45d7daabd9afddaa7c1932bf7d6b3a8688fd
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-04-14 11:17:02 -0300

    main package

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gabrielluizsf/data-structures.go"
+
+func main(){
+	datastructuresgo.PrintMessage("____Estruturas de Dados em Go____")
+	datastructuresgo.Arrays()
+	datastructuresgo.Lists()
+	datastructuresgo.Stack()
+	datastructuresgo.Queue()
+}
